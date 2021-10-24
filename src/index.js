79ca73a9a3d746d76622df4227c4dc36d7ca9172
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-10-24 20:27:26 -0300

    corrigido bug de bloqueio por politicas CORs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const routes = require("./routes");
 const { sequelize } = require('./Model/index');
+const cors = require("cors");
 
 const app = express();
 
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
