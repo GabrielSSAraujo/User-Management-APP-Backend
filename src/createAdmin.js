1bcc1aa2fd03eb192883c2850c9b6e1dd5aead15
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-10-23 23:52:03 -0300

    adicionado script para criacao de super-usuario

---
+++ src/createAdmin.js
@@ -0,0 +1,65 @@
+const {sequelize } = require('./Model/index');
+const bcrypt = require("bcrypt");
+const {user} = require('./Model')
+
+async function hashPassword(pass, saltRounds = 10) {
+	const salt = await bcrypt.genSalt(saltRounds);
+	return bcrypt.hash(pass, salt);
+}
+
+const adminUser = {
+  name: "admin",
+  email: "admin@admin.com",
+  password: "admin",
+  level: 1
+};
+
+function initDatabase(){
+sequelize.sync().then(
+    () => {
+      console.log(`conectado ao banco de dados com sucesso`);
+    }
+);
+}
+
+async function createAdmin() {
+  try {
+    await initDatabase();
+    console.log("admin user created");
+  } catch (err) {
+    console.error(`could not connect to database: ${err}`);
+  }
+
+  console.log("connected to database");
+
+  try {
+    const newAdmin = await user.create({
+      name: adminUser.name,
+      email: adminUser.email,
+      password: await hashPassword(adminUser.password),
+      level: adminUser.level,
+    });
+
+    if (!newAdmin) {
+      console.error("failed to create user");
+      return;
+    }
+    console.log(`created user: ${JSON.stringify(newAdmin)}`);
+  } catch (err) {
+    console.log(`could not create user: ${err}`);
+  }
+}
+
+try {
+  createAdmin().then(
+    () => {
+      process.exit(0);
+    },
+    () => {
+      process.exit(1);
+    }
+  );
+} catch (err) {
+  console.error(`error: ${err}`);
+  process.exit(1);
+}
